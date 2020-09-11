0f843870f5f96981a51ef3c1604dcc63d2e0bfcc
Alwan Mortada <alwan.mortada@silvercar.com>
2020-09-11 10:39:33 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBt251oUkuA_P_WmiIFjtC1I7UJeMdOLeM",
+  authDomain: "store-a5366.firebaseapp.com",
+  databaseURL: "https://store-a5366.firebaseio.com",
+  projectId: "store-a5366",
+  storageBucket: "store-a5366.appspot.com",
+  messagingSenderId: "816379703780",
+  appId: "1:816379703780:web:515bc35275400a2e38d7f1",
+  measurementId: "G-LEJYY0T0QW"
+};
