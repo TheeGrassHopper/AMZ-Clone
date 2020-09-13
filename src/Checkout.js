cba73165016497b05757c7a07cae65a2b4b24cea
Alwan Mortada <alwan.mortada@silvercar.com>
2020-09-13 00:45:39 -0500

    Added login in form and basket list

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Checkout.css';
 import Subtotal from './Subtotal.js';
+import {useStateValue} from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+
+  const[{basket}, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -10,15 +14,21 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
           className="checkout__ad"/>
+
+        <div>
+
+          <h2 className="checkout__title">Your shopping Basket</h2>
+            {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              price={item.price}
+              title={item.title}
+              rating={item.rating}
+              image={item.image}
+            />
+            ))}
+        </div>
       </div>
-
-      <div>
-        <h2 className="checkout__title">
-          Your Shopping Basket
-        </h2>
-
-      </div>
-
       <div className="checkout__right">
         <Subtotal />
         <h2>this is the subtotal </h2>
--- src/reducer.js
+++ src/reducer.js
@@ -20,6 +20,19 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item.price]
       };
+    case 'REMOVE_FROM_BASKET':
+        console.log(state)
+        const index = state.basket.findIndex((item) => item.id === action.id);
+        let newBasket = [...state.basket];
+
+        if (index >= 0) {
+          newBasket.splice(index, 1)
+        } else {
+          console.warn ( `Can't remove product (id: ${action.id} as its not in basket!`)
+        }
+      return {
+          ...state, basket: newBasket
+      };
     default:
       return state;
   }
